8f02502c70d483ebd7c682f8e9f3c8af3837f05a
JDecunha <joeydecunha@gmail.com>
2017-01-15 23:13:46 -0500

    Add files via upload

--- IntroComputing/resume.py
+++ IntroComputing/resume.py
@@ -7,6 +7,14 @@ Created on Mon Jan  9 17:28:03 2017
 
 import numpy
 import matplotlib.pyplot as p
+#Set custom properties of matplotlib
+#matplotlib will interpret all text as LaTeX
+p.rcParams['text.usetex'] = True
+p.rcParams['text.latex.unicode']= False
+p.rcParams['mathtext.fontset'] = 'stix'
+p.rcParams['font.family'] = 'STIXGeneral'
+p.rcParams['figure.autolayout'] = True
+#plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 #from itertools import izip 
 
 
@@ -17,13 +25,13 @@ b = numpy.load("b.npy")
 c = numpy.load("c.npy")
 d = numpy.load("d.npy")
 
-print "Shape of Function a (blue) and b (green)",a.shape
+#print "Shape of Function a (blue) and b (green)",a.shape
 
 # Plot a figure to see what the data looks like
-if True:
+if False:
     p.figure()
-    p.xlabel("Time (s)")
-    p.ylabel("Sample value")
+    p.xlabel(r'Time [s]')
+    p.ylabel(r'Sample value')
     p.errorbar(t,a[:,0],a[:,1],fmt='none',errorevery=3,label="Signal a")
     #p.errorbar(t,b[:,0],b[:,1],fmt='none',errorevery=3)
     #p.errorbar(t,c[:,0],c[:,1],fmt='none',errorevery=3)
@@ -31,11 +39,18 @@ if True:
 
 # once you have written all the required functions, the following
 # should work, and give correct results
-
+"""
 def rms(t,x):
     square = multiply(x,x)
-    mean = integrate(t,square)[1] # not true yet!!!
-    mean[1:,:] /= numpy.column_stack((t,t))[1:,:]
+    mean = integral(t,square)[1] # not true yet!!!
+    mean[1:,:] = numpy.column_stack((t,t))[1:,:]
+    mean[0,:] = 0  # this is probably a matter of dogma
+    return sqrt(mean)
+"""
+def rms(t,x):
+    square = multiply(x,x)
+    mean = integral(t,square) # not true yet!!!
+    mean[1:,:] = numpy.column_stack((t,t))[1:,:]
     mean[0,:] = 0  # this is probably a matter of dogma
     return sqrt(mean)
     
@@ -54,12 +69,12 @@ def add(x,y):
 
 def sqrt(y):
     values = numpy.sqrt(y[:,0])
-    errorbars = values* 0.5* (y[:,1]/y[:,0])
+    errorbars = numpy.sqrt(0.25 * (y[:,1]**2/y[:,0]))
     return numpy.column_stack((values, errorbars))
     
 def multiply(y1,y2):
     values = y1[:,0]*y2[:,0]
-    errorbars = values*(numpy.sqrt((y1[:,1]/y1[:,0])**2 + (y2[:,1]/y2[:,0])**2))
+    errorbars = numpy.sqrt((y1[:,0]*y2[:,1])**2+(y2[:,0]*y1[:,1])**2)
     return numpy.column_stack((values,errorbars))
     
 # Input: t, array of time values; y, array of y values.
@@ -83,32 +98,15 @@ def derivative (t,y):
         # uncertainty calculations
         errY1 = y[x,1]
         errY2 = y[x+1,1]
-        num = numpy.sqrt(numpy.square(errY1)+numpy.square(errY2))
-        denom = ytwo-yone
-        uncerty[x] = num/denom*values[x]/2/(ttwo-tone)
+        #num = numpy.sqrt(numpy.square(errY1)+numpy.square(errY2))
+        #denom = ytwo-yone
+        uncerty[x] = (1/(ttwo-tone))*numpy.sqrt((errY2**2)-(errY1**2))
         
-    toReturn = numpy.concatenate((values,midtime,uncerty))
-    toReturn = numpy.transpose(toReturn)
+    #toReturn = numpy.concatenate((values,midtime,uncerty),axis=1)
+    #toReturn = numpy.transpose(toReturn)
+    toReturn = numpy.column_stack((midtime,values,uncerty))
 
     return toReturn
-    
-# I can verify correctness using known quantity
-## known = 2000*numpy.sqrt(2)*numpy.sin(100*2*numpy.pi*t+numpy.pi/4)
-
-foo = derivative(t,a)
-
-# Plotting derivative graph
-p.figure(figsize=(8,8), dpi=100)
-# x-vals: midtime; y-vals: derivative; error vals: uncertainty
-p.errorbar(foo[:,1],foo[:,0],foo[:,2],fmt='none')
-p.xlabel("Time (s)",fontsize=16)
-p.ylabel("$\frac{d}{dt} (signal) $",fontsize=16)
-
-
-# Will return 512 for number of rows in matrix
-test = (len(a[:,1]))
-print "A Value",test
-
 
 # Input: t, array of time values; y, array of y values.
 # Output: array of output integral values and associated uncertainty 
@@ -140,20 +138,71 @@ def integral(t,y):
     output = numpy.column_stack((values,uncerty))
     
     return output
-    
-    
-bar = integral(t,a)
 
-# p.figure(figsize=(8,4), dpi=100)
-# p.errorbar(t,bar[:,0],0,fmt='none')
+mult = multiply(a,a)
+foo = derivative(t,a)    
+bar = integral(t,a)    
+drms = rms(t,d)
+    
+"""# Plotting Graph of a"""
+p.figure(figsize=(5,4), dpi=150)
+p.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+p.xlabel(r'Time [s]',fontsize=12)
+p.ylabel(r'Sample value',fontsize=12)
+p.errorbar(t,a[:,0],a[:,1],fmt='none',errorevery=1,label="Signal a")
 
-# Plotting integral graph 
-p.figure(figsize=(8,8), dpi=100)
-# x-vals: t; y-vals: integral; error vals: uncertainty
+p.savefig('a.png',dpi=150,pad_inches=0.4) 
+
+"""# Plotting a^2"""
+p.figure(figsize=(5,4), dpi=150)
+p.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+p.xlabel(r'Time [s]',fontsize=12)
+p.ylabel(r'Sample value',fontsize=12)
+p.errorbar(t,mult[:,0],mult[:,1],fmt='none')
+
+p.savefig('asquare.png',dpi=150,pad_inches=0.4) 
+
+""" Plotting derivative of a """
+p.figure(figsize=(5,4), dpi=150)
+p.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+p.xlabel(r'Time [s]',fontsize=12)
+p.ylabel(r'$ \frac{d}{dt} (signal) \left[ \displaystyle\frac{1}{s} \right] $',fontsize=16)
+p.errorbar(foo[:,0],foo[:,1],foo[:,2],fmt='none')
+
+p.savefig('aderiv.png',dpi=150,pad_inches=0.4) 
+
+
+""" Plotting integral of a """
+p.figure(figsize=(5,4), dpi=150)
+p.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+p.xlabel(r'Time [s]',fontsize=12)
+p.ylabel(r'$\int (signal) \ dt \ [s]$',fontsize=12)
 p.errorbar(t,bar[:,0],bar[:,1],fmt='none')
-p.xlabel("Time (s)",fontsize=16)
-p.ylabel("$\int (signal) dt$",fontsize=16)
 
+p.savefig('aintegral.png',dpi=150,pad_inches=0.4) 
+
+""" Plotting Graph of d """
+p.figure(figsize=(5,4), dpi=150)
+p.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+p.xlabel(r'Time [s]',fontsize=12)
+p.ylabel(r'$Sample value$',fontsize=12)
+p.errorbar(t,d[:,0],d[:,1],fmt='none')
+
+p.savefig('d.png',dpi=150,pad_inches=0.4) 
+
+""" Plotting rms of d """
+p.figure(figsize=(5,4), dpi=150)
+p.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+p.xlabel(r'Time [s]',fontsize=12)
+p.ylabel(r'$RMS \ signal$',fontsize=12)
+p.errorbar(t,drms[:,0],drms[:,1],fmt='none')
+
+p.savefig('drms.png',dpi=150,pad_inches=0.4) 
+
+
+
+# I can verify correctness using known quantity
+## known = 2000*numpy.sqrt(2)*numpy.sin(100*2*numpy.pi*t+numpy.pi/4)
     
     
 
