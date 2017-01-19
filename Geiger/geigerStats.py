0a62d80d30c063181ffadf28a6d1ad1a957145f5
Shammamah Hossain <shammamah.hossain@ws339-04.ugrad.physics.mcgill.ca>
2017-01-19 15:24:10 -0500

    Finish row analysis of geiger7 data

--- Geiger/geigerStats.py
+++ Geiger/geigerStats.py
@@ -1,24 +1,23 @@
 import numpy 
 import matplotlib.pyplot as plt 
-%matplotlib inline
+#%matplotlib inline
 
 # 2D array 
 # 128 rows 
 # 25 columns 
 
 #########ROW ANALYSIS##########
-
-# Input: trial, a 1D array of histogram data 
-# Output: mean, mean of the array data
+  
 def mean(trial):
-	tot = 0 
-	numDataPoints = 0
-	for i in range(len(trial)): 
-		# weighted
-		# adding 1 to index to avoid OBO
-		tot+=((i)*trial[i])
-		numDataPoints+=trial[i]
-	return (float)(tot/numDataPoints)
+    tot = 0
+    numDataPoints = 0
+    for i in range(len(trial)):
+        #weighted
+        tot+=((i)*trial[i])
+        numDataPoints+=trial[i]
+    tot = float(tot)
+    numDataPoints = float(numDataPoints)
+    return float(numpy.divide(tot,numDataPoints))
 
 # Input: trial, a 1D array of histogram data; 
 #		 mean, mean of the values in the array 
@@ -29,25 +28,41 @@ def variance(trial, mean):
 		# must be weighted 
 		# adding 1 to index to avoid OBO
 		varSum += (trial[j])*(j-mean)**2
-	return (float)(varSum/(len(trial)-1))
+	return float(numpy.divide(varSum,(len(trial)-1)))
 
-# Load data
-geigerData = numpy.loadtxt('histogram.txt')
-geigerDataTransposed = numpy.transpose(geigerData)
+# Input: trial, a 1D array of histogram data;
+#        var, the variance value for the data
+# Output: stdErr, standard error of the array data
+def rowStdErr(trial,var):
+    N = 0
+    for i in range(len(trial)):
+        N+=trial[i]
+    return numpy.sqrt(var/N)
+        
+        
 
-meanReplica = [mean(geigerData[i]) for i in range(len(geigerData))]
-varData = [variance(geigerData[i],meanReplica[i]) for i in range(len(geigerData))]
-rowStdErr = [(float)(numpy.sqrt(variance(geigerData[i],meanReplica[i]))/numpy.sqrt(len(geigerData))) for i in range(len(geigerData))]
+# Load data from run with average of 7
+geigerData7 = run7
+
+geigerDataTransposed = numpy.transpose(geigerData7)
+
+meanReplica = [mean(geigerData7[i]) for i in range(len(geigerData7))]
+replicaVarData = [variance(geigerData7[i],meanReplica[i]) for i in range(len(geigerData7))]
+replicaStdErr = [rowStdErr(geigerData7[i],replicaVarData[i]) for i in range(len(geigerData7))]
+
+print(replicaStdErr[0:10])
+
+
 
 ############Column Analysis##############
 # Transpose data to put the columns into rows (for ease of use)
-geigerDataTransposed = numpy.transpose(gigerData)
+geigerDataTransposed = numpy.transpose(geigerData)
 # Mean values of each column in the original dataset
-meanCol = [sum(geigerDataTransposed[i])/(len(geigerDataTransposed)) for i in range(len(geigerDataTransposed))]
+# meanCol = [sum(geigerDataTransposed[i])/(len(geigerDataTransposed)) for i in range(len(geigerDataTransposed))]
 # Variances of each column in the original dataset 
-varCol = [(sum(geigerDataTransposed[i]-meanCol[i])**2)/(len(geigerDataTransposed)) for i in range(geigerDataTransposed)] 
+# varCol = [(sum(geigerDataTransposed[i]-meanCol[i])**2)/(len(geigerDataTransposed)) for i in range(geigerDataTransposed)] 
 # Standard error: take variance and mean for each column (calculated above)
-colStdErr = [(float)(numpy.sqrt(varCol[i])/numpy.sqrt(len(geigerDataTransposed))) for i in range(geigerDataTransposed)]
+# colStdErr = [(float)(numpy.sqrt(varCol[i])/numpy.sqrt(len(geigerDataTransposed))) for i in range(geigerDataTransposed)]
 
 # x values for plot 
 guessNumber = []
@@ -71,3 +86,4 @@ plt.suptitle("Mean frequency of number of guesses",fontsize=20)
 plt.xlabel("Number of guesses",fontsize=16)
 plt.ylabel("Mean frequency",fontsize=16)
 plt.xlim(15,26)
+"""
