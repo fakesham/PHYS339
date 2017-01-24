4f142d6d3b6ad0f0cd8309a462ad350e593c3e19
Shammamah Hossain <shammamah.hossain@ws339-04.ugrad.physics.mcgill.ca>
2017-01-24 16:22:14 -0500

    Finish all distributions and chi-squareds for all compressed data sets

--- Maryam/geigerStats1.py
+++ Maryam/geigerStats1.py
@@ -119,11 +119,6 @@ def findGaussian(trials):
             gaussian[j][i] = numpy.sum(trials[j])*scipy.stats.norm.pdf(i,mean(trials[j]),numpy.sqrt(variance(trials[j],mean(trials[j]))))
     return gaussian 
     
-# xvals = numpy.arange(0,22,1) 
-
-poisson128 = findPoisson(geigerData7)
-gaussian128 = findGaussian(geigerData7)
-
 # Input: observed, a 2D array of observed data;
 #        expected, a 2D array of expected values based on a PDF or PMF
 #        variance, a 1D array of the variance in each bin calculated globally 
@@ -139,10 +134,9 @@ def chiSquare(observed,expected,variance):
     
     return chisq
                 
-        
-csp128 = chiSquare(geigerData7,poisson128,colVar) 
-csg128 = chiSquare(geigerData7,gaussian128,colVar)
 
+# Input: data, a 2D array of observed data 
+# Output: a 2D array of observed data, with half the number of trials 
 def compress(data):
     # just compress every two rows
     topHalf = numpy.empty((len(data)/2,len(data[0])))
@@ -151,7 +145,74 @@ def compress(data):
         topHalf[i]=data[i]
     for i in range(0,len(data)/2):
         bottomHalf[i]=data[i+len(data)/2]
-    return numpy.sum(topHalf,bottomHalf) 
+    return numpy.add(topHalf,bottomHalf) 
+
+
+
+# Compressed data sets 
+geigerData7_64 = compress(geigerData7)
+geigerData7_32 = compress(geigerData7_64)
+geigerData7_16 = compress(geigerData7_32)
+geigerData7_8 = compress(geigerData7_16)
+geigerData7_4 = compress(geigerData7_8)
+geigerData7_2 = compress(geigerData7_4)
+geigerData7_1 = compress(geigerData7_2)
+
+# All distributions and chi-squared values for each compressed data set 
+
+# 128 trials
+poisson128 = findPoisson(geigerData7)
+csp128 = chiSquare(geigerData7,poisson128,colVar) 
+gaussian128 = findGaussian(geigerData7)
+csg128 = chiSquare(geigerData7,gaussian128,colVar)
+# 64 trials 
+poisson64 = findPoisson(geigerData7_64)
+csp64 = chiSquare(geigerData7_64,poisson64,colVar) 
+gaussian64 = findGaussian(geigerData7_64)
+csg64 = chiSquare(geigerData7_64,gaussian64,colVar)
+# 32 trials 
+poisson32 = findPoisson(geigerData7_32)
+csp32 = chiSquare(geigerData7_32,poisson32,colVar) 
+gaussian32 = findGaussian(geigerData7_32)
+csg32 = chiSquare(geigerData7_32,gaussian32,colVar)
+# 16 trials 
+poisson16 = findPoisson(geigerData7_16)
+csp16 = chiSquare(geigerData7_16,poisson16,colVar) 
+gaussian16 = findGaussian(geigerData7_16)
+csg16 = chiSquare(geigerData7_16,gaussian16,colVar)
+# 8 trials 
+poisson8 = findPoisson(geigerData7_8)
+csp8 = chiSquare(geigerData7_8,poisson8,colVar) 
+gaussian8 = findGaussian(geigerData7_8)
+csg8 = chiSquare(geigerData7_8,gaussian8,colVar)
+# 4 trials 
+poisson4 = findPoisson(geigerData7_4)
+csp4 = chiSquare(geigerData7_4,poisson4,colVar) 
+gaussian4 = findGaussian(geigerData7_4)
+csg4 = chiSquare(geigerData7_4,gaussian4,colVar) 
+# 2 trials 
+poisson2 = findPoisson(geigerData7_2)
+csp2 = chiSquare(geigerData7_2,poisson2,colVar) 
+gaussian2 = findGaussian(geigerData7_2)
+csg2 = chiSquare(geigerData7_2,gaussian2,colVar)
+# 1 trial
+poisson1 = findPoisson(geigerData7_1)
+csp1 = chiSquare(geigerData7_1,poisson1,colVar) 
+gaussian1 = findGaussian(geigerData7_1)
+csg1 = chiSquare(geigerData7_1,gaussian1,colVar)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 """
