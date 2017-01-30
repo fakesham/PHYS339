1f8dde326141a5d72a9af13bcea83a9fa43aaa05
Shammamah Hossain <shammamah.hossain@ws339-04.ugrad.physics.mcgill.ca>
2017-01-30 16:06:26 -0500

    residual plots for run with 7 average

--- Geiger/geigerStats1.py
+++ Geiger/geigerStats1.py
@@ -277,24 +277,30 @@ plt.legend(fontsize=8)
 exec("plt.savefig('totalbincounts_%s.png',dpi=150,pad_inches=10)"%int(overallMean))
 
 # Residuals - Poisson 
+ye = [numpy.sqrt(totalBinCounts[i]) for i in range(len(totalBinCounts))]
 residuals = numpy.transpose(numpy.subtract(poisson1,totalBinCounts))
 plt.figure(figsize=(8,6), dpi=150)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 plt.xlabel("Total events per interval",fontsize=12)
 plt.ylabel("Residual value \n(predicted frequency - observed frequency)",fontsize=12)
-plt.plot(xvals, residuals,'o')
+plt.errorbar(xvals, residuals, yerr=ye, fmt='o')
+plt.plot([0,25],[0,0])
 plt.legend(fontsize=8)
 exec("plt.savefig('Poissonresiduals_%s.png',dpi=150)"%int(overallMean))
 
+
 # Residuals - Gaussian 
+ye = [numpy.sqrt(totalBinCounts[i]) for i in range(len(totalBinCounts))]
 residuals = numpy.transpose(numpy.subtract(gaussian1,totalBinCounts))
 plt.figure(figsize=(8,6), dpi=150)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 plt.xlabel("Total events per interval",fontsize=12)
 plt.ylabel("Residual value \n(predicted frequency - observed frequency)",fontsize=12)
-plt.plot(xvals, residuals,'o')
+plt.errorbar(xvals, residuals, yerr=ye, fmt='o')
+plt.plot([0,25],[0,0])
 plt.legend(fontsize=8)
 exec("plt.savefig('Gaussianresiduals__%s.png',dpi=150)"%int(overallMean))
+
 
 # Variance vs. mean - Poisson
 plt.figure(figsize=(8,6), dpi=150)
@@ -305,6 +311,7 @@ plt.plot(colMean, colVar,'o')
 plt.legend(fontsize=8)
 exec("plt.savefig('Poissonvarmean_%s.png',dpi=150)"%int(overallMean))
 
+
 # Chi square - Poisson 
 plt.figure(figsize=(8,6), dpi=150)
 plt.hist(csp1024,bins=1024,color='lightgrey')
@@ -313,186 +320,7 @@ x = numpy.linspace(0,max(csp1024),num=512)
 y = numpy.multiply(chi2.pdf(x,df),1024)
 plt.xlabel("Chi-square value",fontsize=12)
 plt.ylabel("Frequency",fontsize=12)
-plt.plot(x,y,color='red')
+plt.plot(x,y,color='red',label="Chi-square distribution for %d degrees of freedom"%df)
+plt.legend(fontsize=8)
 plt.savefig('chisq_1024.png')
 
-# Chi square - Poisson 
-plt.figure(figsize=(8,6), dpi=150)
-plt.hist(csp128,bins=128,color='lightgrey')
-df = totalDOF-1
-x = numpy.linspace(0,max(csp128),num=128)
-y = numpy.multiply(chi2.pdf(x,df),128)
-plt.xlabel("Chi-square value",fontsize=12)
-plt.ylabel("Frequency",fontsize=12)
-plt.plot(x,y,color='red')
-plt.savefig('chisq_128.png')
-
-#################################### UNUSED CODE ####################################
-"""
-# g-chisquare and p-chisquare: difference between actual bin result and predicted Gaussian/Poisson bin result 
-# calculate for each bin (22 bins)
-# 128 x 22
-def chiSquare (data,var,pdf):
-	tot = 0
-	for i in range (len(data)):
-     # ignore if variance is zero 
-		if var(i) != 0:
-			tot = tot + float(((data(i)-pdf(i))**2)/var(i))
-         return tot
-# we get 128/2 lists of 22 chisquares
-# what is the error on each bin ? difficult!!!
-g-chiSquare = [chiSquare(compressed1, replicaVarData, replicaGaussianDist) for i in range(compressed1)] 
-p-chiSquare = [chiSquare(compressed2, replicaVarData, replicaPoissonDist) for i in range(compressed1)]
-
-
-observedBinCounts = numpy.empty(len(gDataTransposed))
-for i in range(len(gDataTransposed)):
-    observedBinCounts[i] = numpy.sum(gDataTransposed[i])
-
- # twice the number of intervals per replica.
-
-    
-compressed1 = compress(gData)  
-
-gData_64 = compress(gData)
-gData_32 = compress(gData_64)
-gData_16 = compress(gData_32)
-gData_8 = compress(gData_16)
-gData_4 = compress(gData_8)
-gData_2 = compress(gData_4)
-gData_1 = compress(gData_2)
-
-
-
-# 128 trials
-cv = colVar
-
-poisson128 = findPoisson(gData)
-csp128 = chiSquare(gData,poisson128,cv) 
-gaussian128 = findGaussian(gData)
-csg128 = chiSquare(gData,gaussian128,cv)
-
-
-# 64 trials 
-cv = cVar(gData_64, colMean)
-
-poisson64 = findPoisson(gData_64)
-csp64 = chiSquare(gData_64,poisson64,cv) 
-gaussian64 = findGaussian(gData_64)
-csg64 = chiSquare(gData_64,gaussian64,cv)
-
-
-# 32 trials 
-cv = cVar(gData_32, colMean)
-
-poisson32 = findPoisson(gData_32)
-csp32 = chiSquare(gData_32,poisson32,cv) 
-gaussian32 = findGaussian(gData_32)
-csg32 = chiSquare(gData_32,gaussian32,cv)
-
-
-# 16 trials 
-cv = cVar(gData_16, colMean)
-
-poisson16 = findPoisson(gData_16)
-csp16 = chiSquare(gData_16,poisson16,cv) 
-gaussian16 = findGaussian(gData_16)
-csg16 = chiSquare(gData_16,gaussian16,cv)
-
-
-# 8 trials 
-cv = cVar(gData_8, colMean)
-
-poisson8 = findPoisson(gData_8)
-csp8 = chiSquare(gData_8,poisson8,cv) 
-gaussian8 = findGaussian(gData_8)
-csg8 = chiSquare(gData_8,gaussian8,cv)
-
-
-# 4 trials 
-cv = cVar(gData_4, colMean)
-
-poisson4 = findPoisson(gData_4)
-csp4 = chiSquare(gData_4,poisson4,cv) 
-gaussian4 = findGaussian(gData_4)
-csg4 = chiSquare(gData_4,gaussian4,cv) 
-
-
-# 2 trials 
-cv = cVar(gData_2, colMean)
-
-poisson2 = findPoisson(gData_2)
-csp2 = chiSquare(gData_2,poisson2,cv) 
-gaussian2 = findGaussian(gData_2)
-csg2 = chiSquare(gData_2,gaussian2,cv)
-
-
-# 1 trial
-cv = cVar(gData_1, colMean)
-
-poisson1 = findPoisson(gData_1)
-csp1 = chiSquare(gData_1,poisson1,cv) 
-gaussian1 = findGaussian(gData_1)
-csg1 = chiSquare(gData_1,gaussian1,cv)
-
-
-
-# we must see that the result is noisy must either increase the number of intervals more than 64 or add replicas together
-#we just repeat the collapse of the data set again compressed2 = compress(compressed1) 
-# we do it consecutively and compare distributions 
-# finally we take chisquare by meanCol,colVar if u want to get only one overall chi square for each bin so a list of 22 elements 
-#g-chiSquare = [chiSquare(meanCol, varCol, replicaGaussianDist) for i in range(meanCol)] 
-#p-chiSquare = [chiSquare(meanCol, varCol, replicaPoissonDist) for i in range(meanCol)]
-# the variance is not it !
-
-                
-xvals = numpy.linspace(0,21,num=22)
-plt.plot(xvals,replicaPoissonDist,'+')
-plt.plot(xvals,observedBinCounts,'o')
-#calculated values
-#plt.plot(xvals,replicaGaussianDist,'o')
-
-
-p128 = gt(poissonChiSq,csp128)
-p64 = gt(poissonChiSq,csp64) 
-p32 = gt(poissonChiSq,csp32) 
-p16 = gt(poissonChiSq,csp16) 
-p8 = gt(poissonChiSq,csp8) 
-p4 = gt(poissonChiSq,csp4) 
-p2 = gt(poissonChiSq,csp2) 
-p1 = gt(poissonChiSq,csp1)
-
-
-print("Poisson distribution for 21 degrees of freedom: \n12.5% of values must be greater than %f\n"%(poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(128,64, p128, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(64,128, p64, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(32,256, p32, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(16,512, p16, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(8,1024, p8, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(4,2048, p4, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(2,4096, p2, poissonChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(1,8192, p1, poissonChiSq))
-
-
-print("\n\n")
-
-g1 = gt(gaussianChiSq, csg1)
-g2 = gt(gaussianChiSq, csg2)
-g4 = gt(gaussianChiSq, csg4)
-g8 = gt(gaussianChiSq, csg8)
-g16 = gt(gaussianChiSq, csg16)
-g32 = gt(gaussianChiSq, csg32)
-g64 = gt(gaussianChiSq, csg64)
-g128 = gt(gaussianChiSq, csg128)
-
-print("Gaussian distribution for 20 degrees of freedom: \n12.5 percent of values must be greater than %f\n"%(gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(128,64, g128, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(64,128, g64, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(32,256, g32, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(16,512, g16, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(8,1024, g8, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(4,2048, g4, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(2,4096, g2, gaussianChiSq))
-print("%d replicas with %d intervals each: %f percent of values greater than %f"%(1,8192, g1, gaussianChiSq))
-
-"""
