e7bcb5e3f3544b8a7d3d40c03ebe1f2d2c2b2751
Shammamah Hossain <shammamah.hossain@gmail.com>
2017-02-02 13:03:44 -0500

    Add code that generates a correct sine wave

---
+++ uControllerCalibration/makePlot.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 16:12:28 2017
+
+@author: mammam
+"""
+
+import numpy 
+xvals = numpy.linspace(0,250,num=26)
+yvals = numpy.loadtxt('~/Dropbox/school/PHYS339/uControllerCalibration/calibrationData.txt')
