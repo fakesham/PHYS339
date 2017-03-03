00328589d8bb8ece3fc5a9c4448cbd12a7768406
Shammamah Hossain <shammamah.hossain@gmail.com>
2017-03-02 23:37:27 -0500

    Add plot for angle differences

--- Laser/brewsterAnalysis.py
+++ Laser/brewsterAnalysis.py
@@ -8,6 +8,7 @@ Created on Thu Feb 16 16:22:09 2017
 import numpy 
 import scipy
 from scipy.optimize import leastsq
+from scipy.stats import norm 
 import matplotlib.pyplot as plt
 import os
 
@@ -22,6 +23,7 @@ for i in range(50):
     exec("brewster%d_1 = numpy.loadtxt('./brewsterAngles/rawdata/brewsterbesteverest%d.txt')[0]"%(i,i*2))
     exec("brewster%d_2 = numpy.loadtxt('./brewsterAngles/rawdata/brewsterbesteverest%d.txt')[1]"%(i,i*2))
 
+# ----------------------------- Getting Brewster angles -----------------------------
 
 def getSpike(data):
 	ratios = []
@@ -54,28 +56,27 @@ for i in range(50):
 angleOnes = numpy.delete(angleOnes,50)
 angleTwos = numpy.delete(angleTwos,50)
 
+# ----------------------------- Compiling/saving processed data -----------------------------
 
 compiledData = numpy.column_stack((angleOnes,angleTwos))
-
 numpy.savetxt('./brewsterAngles/angleData.txt',compiledData)
 
-<<<<<<< HEAD
+# ----------------------------- Comparison with Gaussian  -----------------------------
+
 normedDiff = numpy.subtract(angleOnes,angleTwos)
 numBins = numpy.ceil(numpy.sqrt(len(normedDiff)))
 
 normx = numpy.linspace(min(normedDiff),max(normedDiff),100)
-p = norm.pdf(normx, numpy.mean(normedDiff),numpy.std(normedDiff))
+p = scipy.stats.norm.pdf(normx, numpy.mean(normedDiff),numpy.std(normedDiff))
 pScaled = numpy.divide(p,numpy.sum(p))
 k = numpy.random.choice(normx,size=10000,p=pScaled)
 
-=======
-rootSquareDiff = numpy.sqrt(numpy.square(numpy.subtract(angleOnes,angleTwos)))
->>>>>>> parent of 85313db... Add updated code for Brewster analysis
-
-plt.figure(figsize=(8,6), dpi=150)
-plt.hist(rootSquareDiff,bins=10,normed=True)
-plt.xlabel("Root squared difference between angles (degrees)",fontsize=12)
-plt.ylabel("Frequency",fontsize=12)
-plt.hist(rootSquareDiff,bins=10)
+plt.figure(figsize=(10,5), dpi=150)
+plt.xlabel('Difference between Brewster angles (degrees)',fontsize=12)
+plt.ylabel('Frequency (normalized)',fontsize=12)
+plt.hist(normedDiff,normed=True,color='lightgrey',label='Observed data')
+plt.plot(normx,p,color='blue',label='Gaussian distribution')
+plt.hist(k,normed=True,fill=None,edgecolor='red',label='Data generated from Gaussian distribution')
+plt.legend(fontsize=10,loc='upper left')
 plt.savefig('./brewsterAngles/angleDiff.png')
 
